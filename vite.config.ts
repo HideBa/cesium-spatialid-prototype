23a35997e9d4fb9ebe39fa74865339d68b4e20a9
HideBa <baba.papa1120.ba@gmail.com>
2024-08-24 14:06:21 +0200

    fix build config

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+/// <reference types="vite/client" />
+/// <reference types="vitest" />
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import cesium from "vite-plugin-cesium";
 
-
 export default defineConfig({
-  plugins: [react(), cesium({ rebuildCesium: true }),
-  ],
+  plugins: [react(), cesium({ rebuildCesium: true })],
   resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+    extensions: [".ts", ".tsx", ".js", ".jsx"],
   },
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/test/setup.ts',
+    environment: "jsdom",
+    setupFiles: "./src/test/setup.ts",
   },
   server: {
     port: 3000,
   },
-})
+});
